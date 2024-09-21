19dc0f6fdaec4ea8a2c95f018a29e4703c336d6d
BeKibo <73799556+BeKibo@users.noreply.github.com>
2024-09-21 16:49:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -96,3 +96,30 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1000); // Attendre 1 seconde après la disparition pour cacher le loader
     }, 10000); // Attendre 5 secondes avant de masquer le loader
 });
+
+
+
+// JavaScript Countdown
+const countdown = () => {
+    const countDate = new Date('Oct 07, 2024 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(gap / day);
+    const hours = Math.floor((gap % day) / hour);
+    const minutes = Math.floor((gap % hour) / minute);
+    const seconds = Math.floor((gap % minute) / second);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+};
+
+// Update countdown every second
+setInterval(countdown, 1000);
